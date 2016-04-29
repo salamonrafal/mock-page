c8684d863c6cc9d86ff31f42ce6e4a341c21871a
Rafal Salamon <rasa@salamonrafal.pl>
2016-04-29 23:28:30 +0200

    Add preprocesing

--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -1,12 +1,17 @@
 var gulp        = require('gulp'),
-    include     = require('gulp-include');
+    include     = require('gulp-include'),
+    preprocess = require('gulp-preprocess');
 
 gulp.task("build:html:editor", function() {
   console.log("-- gulp is running task 'scripts'");
 
   return gulp.src("sources/editor.html")
     .pipe(include())
-      .on('error', console.log)
+    .pipe(preprocess({context: { 
+		class_main: 'mp-container', 
+		class_edit: 'editable'
+	}})) 
+    .on('error', console.log)
     .pipe(gulp.dest("client/"));
 });
 
@@ -15,7 +20,7 @@ gulp.task("build:html:view", function() {
 
   return gulp.src("sources/view.html")
     .pipe(include())
-      .on('error', console.log)
+    .on('error', console.log)
     .pipe(gulp.dest("client/"));
 });
  
