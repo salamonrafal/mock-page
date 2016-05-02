3f7cc6565f9a74c255760147179654a9d1b3c402
Rafal Salamon <rasa@salamonrafal.pl>
2016-05-02 05:55:07 +0200

    Add first block

--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -1,16 +1,22 @@
 var gulp        = require('gulp'),
     include     = require('gulp-include'),
-    preprocess = require('gulp-preprocess');
+    preprocess = require('gulp-preprocess')
+    
+    themeVariables = {
+		class_main: 'mp-container', 
+		class_page: 'mp-default',
+		class_edit: ''
+	};
 
 gulp.task("build:html:editor", function() {
   console.log("-- gulp is running task 'scripts'");
 
+  themeVariables.class_edit = 'js-editable';
+  themeVariables.class_page = 'mp-page-edit';
+  
   return gulp.src("sources/editor.html")
     .pipe(include())
-    .pipe(preprocess({context: { 
-		class_main: 'mp-container', 
-		class_edit: 'editable'
-	}})) 
+    .pipe(preprocess({context: themeVariables})) 
     .on('error', console.log)
     .pipe(gulp.dest("client/"));
 });
@@ -18,8 +24,11 @@ gulp.task("build:html:editor", function() {
 gulp.task("build:html:view", function() {
   console.log("-- gulp is running task 'scripts'");
 
+  themeVariables.class_page = 'mp-page-view';
+  
   return gulp.src("sources/view.html")
     .pipe(include())
+    .pipe(preprocess({context: themeVariables})) 
     .on('error', console.log)
     .pipe(gulp.dest("client/"));
 });
