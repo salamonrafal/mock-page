480684f761e607d31c190361bf63009f74bb838c
Rafal Salamon <rasa@salamonrafal.pl>
2016-05-09 03:31:12 +0200

    Add new blocks & update CSS

--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -5,13 +5,20 @@ var gulp        = require('gulp'),
     themeVariables = {
 		class_main: 'mp-container', 
 		class_page: 'mp-default',
-		class_edit: ''
+		class_edit_flag: '',
+		class_content_blocks: 'mp-content-blocks',
+		class_content_blocks_table: 'mp-content-block-table',
+		class_block_simple: 'mp-block-simple',
+		class_block_2cell: 'mp-block-2cell',
+		class_block_1cell: 'mp-block-1cell',
+		class_block_title: 'mp-block-title',
+		class_block_content: 'mp-block-content'
 	};
 
 gulp.task("build:html:editor", function() {
   console.log("-- gulp is running task 'scripts'");
 
-  themeVariables.class_edit = 'js-editable';
+  themeVariables.class_edit_flag = 'js-editable';
   themeVariables.class_page = 'mp-page-edit';
   
   return gulp.src("sources/editor.html")
